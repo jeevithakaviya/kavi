dcf740dc5576afdf6c8ce50e1415d98ff8a4619f
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-21 11:20:32 +0530

    Create prime number .c

---
+++ prime number .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int low,high,i,flag;
+printf("enter the number intervals");
+scanf("%d%d",&low,&high);
+printf("prime numbers between%dand%d are,"low,high);
+while(low<high)
+{
+flag=0;
+for(i=2;i<=low/2;i++)
+{
+if(low5%==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+printf("%d",low);
+++low;
+}
+return 0;
+}
+
+
+
