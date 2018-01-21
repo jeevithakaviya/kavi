8d195df96bcc73a0c9466155facdf9ceeaa95467
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-21 11:43:15 +0530

    Create strong.c

---
+++ strong.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int low,high,i,temp1,tem2,reminder;
+printf("enter the two number (int):");
+scanf("%d%d,&low&high);
+printf("arm strong between number%%d:",low,high);
+for(i=low+1;i<high;++i)
+{
+temp2=i;
+temp1;
+while(temp1!=0)
+{
+temp/=10;
+++n;
+}
+while(temp2!=0)
+{
+remainder =tem2%10;
+result+=pow(remainder,n);
+temp2/=10;
+}
+if(result==i)
+printf("%d",i);
+n=0;
+result=0;
+}
+return0;
+}
+
+
+
