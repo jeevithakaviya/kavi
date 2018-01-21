c2455afab400947ee72fbac3cfd9398e6f540fb6
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-21 09:48:23 +0530

    Create CON.C

---
+++ CON.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+charc;
+printf("enter the alphats");
+scanf("%d,&c);
+if(c=='a'||c=='e'||c=='o'||c=='u');
+{
+printf("it is vowel");
+}
+else
+{
+printf("constants");
+}
+return o;
+
