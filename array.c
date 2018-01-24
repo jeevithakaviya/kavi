732af73cf05ce189f8554e2b163b37c33dacddb2
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-24 16:02:09 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+inta[15];
+int s,i,m;
+printf("enter the size");
+scanf("%d",&s);
+printf("enter the array");
+for(i=0;i<s;i++);
+{
+scanf("%d",&a[i])
+}
+max=max[0]
+for(i=0;i,s;i++)
+{
+if(maxa[i])
+max=a[i];
+pritf("enter the maxmium value");
+}
+}
+
+ 
+
+
