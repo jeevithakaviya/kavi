c4302d331ad804a58cf34e89d49830ec08bb451a
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-24 16:18:06 +0530

    Create arraysorted.c

---
+++ arraysorted.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+inti,n,t,a[i],j;
+pintf("enter the value");
+scanf("%d",&n);
+printf("enter the values");
+for(i=0;i<n;i++);
+{
+scanf("%d",&a[i]);
+}
+for(i=j+n;j<n;j++)
+{
+ifa[i]>a[j]
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+printf("sorted elements");
+for(i=0;i<n;i++);
+{
+printf("a[i]");
+}
+}
+
