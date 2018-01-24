1600a382e7d892edeb95d3dda014e6715ac8f7d1
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-24 16:07:10 +0530

    Create arraymini.c

---
+++ arraymini.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+void main()
+{
+inta[15];
+int s,i,m;
+printf("enter the size");
+scanf("%d",&s);
+printf("enter the array");
+for(i=0;i<s;i++);
+{
+scanf("%d",&a[i])
+}
+min=min[0]
+for(i=0;i,s;i++)
+{
+if(mina[i])
+min=a[i];
+pritf("enter the minimumvalue");
+}
+}
+
+ 
