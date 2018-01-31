bfcbff8660dcb2031babf113ed814f19d60d8785
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-31 15:42:26 +0530

    Rename array.c to ARRA.C

---
+++ ARRA.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,b[10],c;
+printf("enter no of elements in an array");
+scanf("%d",&c);
+printf("enter the elements in an array");
+for(j=0;j<c;i++)
+{
+scanf("%d\n",&b[j]);
+}
+for(j=0;j<b;j++)
+{
+printf("b[%d]=%d\n",j,b[j]);
+}
+}
