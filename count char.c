f5f25991d14446be37cd4fb9de6a2bcbe7a8b7fb
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-31 16:39:16 +0530

    Create count char.c

---
+++ count char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+intchar[10],j,a,i;
+int count=0;
+for(j=0;j<12;j++)
+{
+printf("enter the strings");
+scanf("%d",i);
+for(a=0;a[i]!=0;n++);
+if(a[i]=1)
+{
+count++
+}
+
