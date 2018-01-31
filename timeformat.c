e58883eb8ca53425d259c682a1aa0ede9b88ebf6
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-01-31 16:04:48 +0530

    Create timeformat.c

---
+++ timeformat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h,m,a;
+printf("enter the times in hours and mintue");
+scanf("%d",& hours);
+printf("%d",&mintues);
+c=h-m;
+printf("enter the subract values");
+}
