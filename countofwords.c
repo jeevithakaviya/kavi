fedf0f27baf438624fc9a71c3726d1458805b877
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-02 15:48:55 +0530

    Create countofwords.c

---
+++ countofwords.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[200];
+int count 0;
+printf("enter the string"):
+scanf("%d",s);
+for(i=0;s[i]!=0;i++)
+{
+if(s[i]==1)
+{
+count++
+}
+printf("no of words in give trings");
+}
