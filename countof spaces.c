32b191f60042c97a8ccc3aa706dac65adf80da7f
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-02 16:00:06 +0530

    Create countof spaces.c

---
+++ countof spaces.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i,spaces;
+chara[30];
+int count 0;
+printf("enterthe string");
+scanf("%d",&s);
+for(i=0;s[i]!=0;i++)
+{
+if(a[i]==1;)
+{
+space++
+}
+printf("no of the spaces in given string");
+}
+
+
