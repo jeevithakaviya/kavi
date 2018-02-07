0c381337242370ad804f55298fe6da27bbadc418
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-07 15:46:36 +0530

    Create MAX.C

---
+++ MAX.C
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+int t,max,s;
+scanf("%d",&s);
+printf("enter the number");
+for(i=0;i<=s;i++)
+scanf("%d",&a[i]);
+max=a[i];
+if(max<a[i])
+{
+max=a[i];
+}
+printf("%d",max);
+}
