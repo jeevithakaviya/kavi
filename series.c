26549e99d2989b0ac45c65d8e92aef75159556ee
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-07 15:53:32 +0530

    Create series.c

---
+++ series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int j, m, c = 2, d = 2, next;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &m);
+
+    printf("Fibonaccin of Series: ");
+
+    for (j = 1; j <= n; ++i)
+    {
+        printf("%d, ", c);
+        next = c + d;
+        c=d;
+        d = next;
+    }
+    return 0
+    }
+    
