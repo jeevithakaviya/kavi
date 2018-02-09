d5c0948e656427e8a876c946611ff6137fc2797a
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-09 16:37:37 +0530

    Create largestnum.c

---
+++ largestnum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,j,t,n;
+for(i=0;i<n;i++)
+{
+	scanf("%d",a[i])
+}
+for(j=i-1;j<n;j++)
+{
+	t=a[i];
+	a[i]=a[i];
+	a[j]=t;
+	
+}
+if(a[i]<a[j])
+{
+	printf("largest number\n",a[j])
+}
+return 0;
+}
