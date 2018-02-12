b450970ef36341848556f239b7788ae7c8c19c5b
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-12 16:00:07 +0530

    Create 48s.c

---
+++ 48s.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+void main()
+{
+    int a[10],k,r,s=0,average;
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+for(i=0;i<k;i++)
+{
+    s=s+a[i];
+    average=s/k;
+    
+}
+    printf("%d",average);
+
+    include <stdio.h>
+
+}
+    
+}
+
+
+
