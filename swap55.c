dc4be2964c05be6d5ef9d6506245350f4ca6ff98
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-12 16:32:08 +0530

    Create swap55.c

---
+++ swap55.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void swap()
+{
+int a,b;
+{
+t=a;
+a=b;
+b=t;
+}
+printf("enter the value of a");
+scanf("%d",&c);
+printf("enter the value of b");
+scanf("%d",&d);
+swap(c,d);
+printf("swap of two numbers ");
+}
