14e4653a3e9b7d9cbd0fb56d2fb087c563385781
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-12 16:16:53 +0530

    Create 42string.c

---
+++ 42string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+chars[20],chart[50];
+int i;
+printf("\enter two strings");
+scanf(%s%s",s,t);
+i=0;
+while(s[i]==t[i]&&s[i]!='\0')
+i++;
+if(s[i]>t[i])
+printf("%S",s);
+elseif(s[i]<t[i])
+{
+printf("%s",t);
+}
+else
+{
+printf("string is %s",s);
+}
+}
