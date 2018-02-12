d9d367b785b2ba50065ae511e56a483dcb82e6cd
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-12 16:40:52 +0530

    Create 10max.c

---
+++ 10max.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+void main()
+{
+int i=123456789,max,t;
+scanf("%d",&t);
+printf("enter the number");
+for(i=0;i<=t;i++)
+scanf("%d",&a[i]);
+max=a[i];
+if(max<a[i])
+{
+max=a[i];
+}
+printf("%d",max);
+}
