f14548bd928d2a889707ca4f9acb7ed48a465195
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-19 16:10:46 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,17 @@
+#include<stdo.h>
+int main()
+{
+
+    int a=6,b=9,c;
+    c=a+b;
+    printf("sum of the numbers%d",c);
+    if(c%2==0)
+    {
+        printf("even number%d",c);
+    }
+    else
+    {
+        printf("odd number%d",c);
+    }
+    return 0()
+}
