c8a92b8e5f9d86c297d24d00f9cdef0e299d82d9
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-19 16:00:43 +0530

    Create addnmber.c

---
+++ addnmber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=6,b=9,c;
+    c=a+b;
+    printf("sum of the numbers%d",c);
+    if(c%2==0)
+    {
+        printf("even number%d",c);
+    }
+    else
+    {
+        printf("odd number%d",c);
+    }
+    return 0()
+}
