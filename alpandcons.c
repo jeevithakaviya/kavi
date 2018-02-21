9fd02b61d2ba6c9c3dd72b57dd1aa1ebf60a407e
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-21 16:05:46 +0530

    Create alpandcons.c

---
+++ alpandcons.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[10]=lap1223;
+scanf("%s",&a);
+	if((a[10]>='a')&&(a[10]<='z')&&(a[10]>='A')&&(a[10]<='Z')&&(a[10]>='0')&&(a[10]<='9'))
+	{
+		printf("yes%d");
+	}
+	else
+	{
+		printf("no%d");
+    }
+    return 0
+    )
+    
