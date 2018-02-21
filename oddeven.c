9c3259230938c220b772cbf2bd8866752775cc42
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-21 16:10:13 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,17 @@
+#include<stdo.h>
+void main()
+{
+
+    int a=5,b=10,c;
+    c=a+b;
+    printf("sum of the numbers%d",c);
+    if(c%2==0)
+    {
+        printf("even number%d",c);
+    }
+    else
+    {
+        printf("odd number%d",c);
+    }
+   
+}
