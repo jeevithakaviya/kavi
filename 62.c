c97e0ae1709af3289e6d698b54daff89794a286f
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-28 15:36:32 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b;
+    printf("\n enter the number");
+    scanf("%d",&b);
+    while(b%10!=0)
+    {
+        b++;
+    }
+printf(" the nearest greater multiple of 10 is \n%d",a);
+return 0;
+}
