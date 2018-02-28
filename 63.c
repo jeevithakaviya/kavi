7c67d3aa543f8855cede6b5d4772c623fcfeae4c
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-02-28 15:42:41 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,19 @@
+include<stdio.h>
+#include<string.h>
+int main()
+{
+  int j,flag=0;
+  char b[20];
+  printf("enter the string");
+  scanf("%s",b);
+  for(j=0;b[i]!='\0';j++)
+  {
+ if((b[i]=='0')||(b[i]=='1'))
+ {
+   flag++;
+ }
+}
+if(flag!=0)
+ printf("yes");
+ 
+  }
