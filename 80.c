7b833187a4499cd2a15683900ff999cb89e62e69
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-03-14 16:23:40 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int value,t,k,ans=0;
+	printf("enter the number");
+	scanf("%d",&v);
+	t=v;
+	while(t>0)
+	{
+		r=t%10;
+		a=(a*10)+k;
+		t=t/10;
+	}
+	
+		while(a>0)
+	{
+		k=ans%10;
+	if(r%2==0);
+	else
+	printf("%d\t",r);
+		a=a/10;
+	}
+	}
