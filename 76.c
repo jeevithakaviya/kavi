d13a8c7871f05a815fa4b60b2236f28780c4cd18
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-03-14 16:17:16 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+void main()
+{
+int num,i,flag=0;
+scanf("%d",&num);
+for(i=2;i<=num/2;i++)
+{
+    if(num%i==0)
+    {
+        flag=1;
+        break;
+        
+    }
+}
+if(flag==0)
+{
+    printf("no");
+}
+else
+{
+    printf("yes");
+}
+}
+
+
