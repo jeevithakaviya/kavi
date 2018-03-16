c93977f2f77bbe65b8e3a3037f9e064fd204d508
jeevithakaviya <35262679+jeevithakaviya@users.noreply.github.com>
2018-03-16 15:57:35 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char c[100];
+int b,j;
+printf("\n enter the string");
+scanf("%[^\n]s",&b);
+c=strlen(b);
+printf("\n the numbers is given string are");
+for(j=0;j<l;j++)
+{
+if(b[i]==0||b[i]==1||b[i]==2||b[i]==3||b[i]==4||b[i]==5||b[i]==6||b[i]==7||b[i]==8||b[i]==9)
+{
+		printf("%d",b[i]);
+
+}
+}
+	return 0;
+}
